7dd0def4ce7ff58f82c2b931f9ec65cee5cc4de5
xbabalov <t.babalova.17@gmail.com>
2024-10-08 02:44:38 +0200

    e2e tests added
    Signed-off-by: xbabalov <t.babalova.17@gmail.com>

--- tests/src/model/pages/developer-sandbox-page.ts
+++ tests/src/model/pages/developer-sandbox-page.ts
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (C) 2023 Red Hat, Inc.
+ * Copyright (C) 2024 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,28 +16,11 @@
  * SPDX-License-Identifier: Apache-2.0
  ***********************************************************************/
 
-/**
- * Config for tests
- * @type {import('vite').UserConfig}
- * @see https://vitest.dev/config/
- */
-const config = {
-  test: {
-    globals: true,
-    coverage: {
-      include: ['**/*.ts'],
-      exclude: ['tests/**', '**/builtin/**',
-        '**/node_modules/**',
-        '**/dist/**',
-        '**/.{idea,git,cache,output,temp,cdix}/**',
-        '**/{.electron-builder,babel,changelog,docusaurus,jest,postcss,prettier,rollup,svelte,tailwind,vite,vitest*,webpack}.config.*',],
-    },
-  },
-  resolve: {
-    alias: {
-      '@podman-desktop/api': `${__dirname}/__mocks__/@podman-desktop/api.js`,
-    },
-  },
-};
+import type { Page } from '@playwright/test';
+import { ExtensionDetailsPage } from '@podman-desktop/tests-playwright';
 
-export default config;
+export class DeveloperSandboxPage extends ExtensionDetailsPage {
+  constructor(page: Page) {
+    super(page, 'Red Hat OpenShift Sandbox extension');
+  }
+}
