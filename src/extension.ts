a19b91b0ddf9143f38ff28a24631a2cbcabeab08
Denis Golovin <dgolovin@redhat.com>
2025-03-17 22:59:54 -0700

    fix: use b&w icon as dark to have better contrast in status bar
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- src/extension.ts
+++ src/extension.ts
@@ -181,11 +181,11 @@ export async function activate(extensionContext: extensionApi.ExtensionContext):
     id: 'redhat.sandbox',
     status,
     images: {
-      icon,
-      logo: {
-        dark: icon,
-        light: icon,
+      icon: {
+        dark: './statusbar-icon.png',
+        light: './icon.png'
       },
+      logo: icon,
     },
     emptyConnectionMarkdownDescription:
       'A free, private OpenShift environment including one project and a resource quota of 14 GB RAM, and 40 GB storage. It lasts 30 days.\n\nSign up at [https://developers.redhat.com/developer-sandbox](https://developers.redhat.com/developer-sandbox/?sc_cid=7013a000003SUmgAAG).',
